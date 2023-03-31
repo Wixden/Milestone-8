8f4683ac8c31b46c8989e55f93ea39c92a3c6190
Wixden <watchonlytutorial@gmail.com>
2023-03-31 18:45:29 +0600

    Milestone 8 Added

---
+++ Module-46/destructuring/js/app.js
@@ -0,0 +1,44 @@
+//API:  "https://restcountries.com/v3.1/all";
+const loadCountries = async () => {
+  // Fetch Option
+  // fetch("https://restcountries.com/v3.1/all")
+  //   .then((res) => res.json)
+  //   .then((data) => console.log(data));
+  // Async Option
+  const url = "https://restcountries.com/v3.1/all";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCountries(data);
+};
+
+const displayCountries = (countries) => {
+  const countryHTML = countries.map((country) => countryHTMLElement(country));
+  const countryContainer = document.getElementById("country__container");
+
+  // countryBody.innerHTML = countryHTML;
+  countryContainer.innerHTML = countryHTML.join(" ");
+};
+
+const countryHTMLElement = (country) => {
+  return `
+  <div class="card shadow-xl my-10 border px-5 py-10">
+  <figure>
+    <img class="rounded-lg h-36 w-72"
+      src="${country.flags.png}"
+      alt="Album"
+    />
+  </figure>  
+  <div class="card-body items-center text-center">
+    <p><b>Name:</b> ${country.name.common}</p>
+    <p><b>Official Name:</b> ${country.name.official} </p>
+    <div class="card-actions justify-start mt-5">
+      <button class="px-10 font-semibold bg-blue-600 py-3 rounded-md text-white">
+        Details
+      </button>
+    </div>  
+  </div>
+  </div>
+  `;
+};
+
+loadCountries();
